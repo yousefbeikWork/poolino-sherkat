a36d7215e4e4428a716800e41d4ca65d7310fd51
yousefbeikWork <110413678+yousefbeikWork@users.noreply.github.com>
2024-11-22 22:51:47 +0330

    navbar created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Headers from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Headers />
+    </div>
+  );
+}
+
+export default App;
